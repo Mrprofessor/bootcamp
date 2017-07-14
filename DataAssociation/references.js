70f38983ae8edc2bca39803f720da8c8651e931c
Mrprofessor <kar.rudra008@gmail.com>
2017-07-14 21:07:52 +0530

    Day Out

--- DataAssociation/references.js
+++ DataAssociation/references.js
@@ -1,64 +1,45 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/blog_demo_2', {useMongoClient: true});
 
+const Post = require('./models/post');
+const User = require('./models/user');
 
-// POST - title, content
-var postSchema = new mongoose.Schema({
-	title : String,
-	content : String,
-});
-var Post = mongoose.model('Post', postSchema);
-
-
-
-// USER - email, name
-var userSchema = new mongoose.Schema({
-	email : String,
-	name : String,
-	posts : [
-			{
-				type : mongoose.Schema.Types.ObjectId,
-				ref  : 'Post'
-			}
-	]
-});
-var User = mongoose.model('User', userSchema);
 
 // User.create({
 // 	email : 'bob@gmail.com',
 // 	name  : 'Bob Belcher'
 // });
 
-// Post.create({
-// 	title : 'Post3 : How to cook the best burger ',
-// 	content: 'Gibberish Gibberish Gibberish Gibberish Gibberish Gibberish'
-// },	function(err, post){
-// 		User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
-// 			if (err) {
-// 				console.log(err);
-// 			} else {
-// 				foundUser.posts.push(post);
-// 				foundUser.save(function(err, data){
-// 					if (err) {
-// 						console.log(err);
-// 					} else {
-// 						console.log(data);
-// 					}
-// 				});
-// 			}		
-// 		});
-// });
+Post.create({
+	title : 'Post4 : How to cook the best burger ',
+	content: 'Star Wars fcsdfsdsd srdgdr hdfh dfh hftghftg'
+},	function(err, post){
+		User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
+			if (err) {
+				console.log(err);
+			} else {
+				foundUser.posts.push(post);
+				foundUser.save(function(err, data){
+					if (err) {
+						console.log(err);
+					} else {
+						console.log(data);
+					}
+				});
+			}		
+		});
+});
 
 
 // FIND USER
 // FIND ALL POSTS FOR THAT USER
 
-User.findOne({email: 'bob@gmail.com'}).populate('posts').exec(function(err, user){
+// User.findOne({email: 'bob@gmail.com'}).populate('posts').exec(function(err, user){
 
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(user);
-	}
-});
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(user);
+// 	}
+// });
